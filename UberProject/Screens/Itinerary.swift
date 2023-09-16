a4d6d613eaf428ef0c02581254643e770b77a715
Tyler Xiao <yxiao1717@gmail.com>
2023-09-16 16:06:42 -0700

    added features

---
+++ UberProject/Screens/Itinerary.swift
@@ -0,0 +1,29 @@
+//
+//  Itinerary.swift
+//  UberProject
+//
+//  Created by Tyler Xiao on 9/16/23.
+//
+
+import UIKit
+
+class Itinerary: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
