f8f1791d58b2f2234e91b40e4ff49823cdca3196
William Wu <wutwilliam@gmail.com>
2023-09-16 20:04:12 -0700

    GooglePlacesManager in progress

--- UberProject/Screens/GooglePlacesManager.swift
+++ UberProject/Screens/GooglePlacesManager.swift
@@ -9,9 +9,19 @@ import Foundation
 import GooglePlaces
 
 final class GooglePlacesManager {
-    //static let shared = GooglePlacesManager()
-    //private let client =
-    private init() {
-        
+    static let shared = GooglePlacesManager()
+    
+    private let client = GMSPlacesClient.shared()
+    
+    private init() {}
+    
+    public func setUp() {
+        GMSPlacesClient.provideAPIKey(Bundle.main.infoDictionary?["API_KEY"] as? String)
+    }
+    
+    public func findPlaces(
+        query: String,
+        completion: @escaping (Result<[String], Error>) -> Void) {
+            
     }
 }
