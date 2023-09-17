9bd1718a86483251499458979264962bbb8f9d6d
Aidan Wu <aidanywu@gmail.com>
2023-09-17 13:18:12 -0700

    limited edits

--- UberProject/Screens/TestingScreen.swift
+++ UberProject/Screens/TestingScreen.swift
@@ -12,10 +12,10 @@ class TestingScreen: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         var routes = [String: Any]()
-        routes = fetchRoutes(orgLat: 37.419734, orgLong: -122.0827784, desLat: 37.417670, desLong: -122.079595)
+        routes = fetchRoutes(orgAdd: "1665 E 7th St, Brooklyn, NY 11230", desAdd: "93-02 69th Ave, Queens, NY 11375")
         print(routes)
         print("\n--------------------------------------\n")
-        routes = fetchRoutes(orgAdd: "1600 Amphitheatre Parkway, Mountain View, CA", desAdd: "450 Serra Mall, Stanford, CA 94305, USA")
+        routes = fetchRoutes(orgAdd: "Portland, Oregon", desAdd: "Chicago, Illinois")
         print(routes)
         // Do any additional setup after loading the view.
     }
