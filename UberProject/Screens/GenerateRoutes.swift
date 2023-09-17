cf5212f83d538747072745aab32af1898ea51476
e-shen2022 <aimashen.2017@gmail.com>
2023-09-17 13:16:44 -0700

    generate routes

--- UberProject/Screens/GenerateRoutes.swift
+++ UberProject/Screens/GenerateRoutes.swift
@@ -8,16 +8,9 @@
 import UIKit
 
 class GenerateRoutes: UIViewController {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    
 }
